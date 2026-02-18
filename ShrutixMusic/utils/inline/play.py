7f7d19dce0000cef8f7da870550d7c2e1139864f
ixbittu <shivamlkr628@gmail.com>
2026-02-18 22:51:12 +0530

    Update play.py

--- ShrutixMusic/utils/inline/play.py
+++ ShrutixMusic/utils/inline/play.py
@@ -67,7 +67,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
-        InlineKeyboardButton(text="• ᴍαɢɪᴄ ✨•", url="https://t.me/ytxmusic_bot?start=_tgr_JCUTUrY0YjQ1")],
+        InlineKeyboardButton(text="⌯ ʌᴅᴅ ϻє ⌯", url="https://t.me/Rossy_musicbot?startgroup=true")],
     ]
     return buttons
 
@@ -81,7 +81,8 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+        InlineKeyboardButton(text="⌯ ʌᴅᴅ ϻє ⌯", url="https://t.me/Rossy_musicbot?startgroup=true")],
     ]
     return buttons
 
